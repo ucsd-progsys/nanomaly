ff10033c401976858892cf12284f565d3539e5a9
Eric Seidel <gridaphobe@gmail.com>
2015-11-16 16:48:15 -0800

    intro

---
+++ paper/pldi16-submission/fac.ml
@@ -0,0 +1,5 @@
+let rec factorial n =
+  if n <= 0 then
+    true
+  else
+    n * factorial (n-1)
---
+++ paper/pldi16-submission/fac.py
@@ -0,0 +1,5 @@
+def factorial(n):
+  if n <= 0:
+    return True
+  else:
+    return n * factorial(n - 1)
