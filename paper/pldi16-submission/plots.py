ac49a1fd71935b68a16f1792c380e528252abc7e
Eric Seidel <gridaphobe@gmail.com>
2015-11-17 11:01:00 -0800

    trace size graph for seminal

--- paper/pldi16-submission/plots.py
+++ paper/pldi16-submission/plots.py
@@ -46,6 +46,44 @@ def plot_coverage(data):
 
     plt.show()
 
+def plot_trace_size(data, label):
+    # xy = cumulative_coverage(data)
+
+    # N = len(xy)
+    # ind = np.arange(N)    # the x locations for the groups
+    # width = 0.5       # the width of the bars: can also be len(x) sequence
+
+    # p1 = plt.bar(ind, [r[1] for r in xy], width,
+    #              color=COLORS[0])
+
+    step = [int(r[5]) for r in data[1:] if r[4] in UNSAFE and int(r[5]) > 0]
+    jump = [int(r[6]) for r in data[1:] if r[4] in UNSAFE and int(r[6]) > 0]
+    
+    bins = [0, 5, 10, 20, 50, 100, 1000]
+    binlabels = ['<5', '5-9', '10-19', '20-49', '50-99', '>=100']
+    ind = np.arange(1, len(bins))
+    width = 0.35
+
+    fig, ax = plt.subplots()
+
+    y,binEdges=np.histogram(step,bins=bins)
+    p1 = ax.bar(ind, y, label='Steps', width=width, color=COLORS[0])
+
+    y,binEdges=np.histogram(jump,bins=bins)
+    p2 = ax.bar(ind+width, y, label='Jumps', width=width, color=COLORS[1])
+    # p1 = plt.bar(0.5*(binEdges[1:]+binEdges[:-1]), y, label='Steps')
+    # p1 = plt.hist([step,jump], bins=bins, label=['Steps', 'Jumps'], range=(0,300), color=COLORS[:2])
+
+    # plt.xlabel('Size')
+    plt.ylabel('Traces')
+    plt.title('Size of generated traces (%s)' % label)
+    plt.xticks(ind + width, binlabels)
+    # plt.yticks(np.arange(0.0, 1.1, 0.1))
+    # plt.legend((p1[0],), ('Seminal',))
+    plt.legend((p1[0], p2[0]), ('Steps', 'Jumps'))
+
+    plt.show()
+
 def plot_distrib(data, label):
     data = data[1:]
     rs = [len([r for r in data if r[4] == o])
@@ -58,6 +96,8 @@ def plot_distrib(data, label):
     plt.axes(aspect=1)
     p1 = plt.pie(rs, labels=ALL_L,
                  autopct='%.1f%%',
+                 pctdistance=1.15,
+                 labeldistance=10,
                  colors=COLORS,
                  shadow=True)
 
@@ -73,3 +113,4 @@ def plot_distrib(data, label):
     # plt.legend((p1[0], p2[0]), ('Men', 'Women'))
 
     plt.show()
+
